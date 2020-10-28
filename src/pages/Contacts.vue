3665f6e6b0fd660a8f95ed550a7df1233dcb32ce
Jack Elliott <jackthename@gmail.com>
2020-10-28 00:34:42 -0700

    adj to Contacts page

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -395,75 +395,53 @@ export default {
       search: '',
       contacts_list: [
         {
-          name: 'Pratik Patel',
+          name: 'Jack Elliott',
           position: 'Developer',
           avatar: 'https://avatars2.githubusercontent.com/u/34883558?s=400&v=4',
-          email: 'pratikpatelpp802@gmail.com',
-          company_email: 'pratikpatelpp802@gmail.com',
+          email: 'jackthename@gmail.com',
+          company_email: 'ursinedesign@gmail.com',
           website: 'www.test.com',
-          phone: '+9910101010',
-          secondary_phone: '+9910101010',
-          address: 'BB 101 Om Sai Residency Palsana'
+          phone: '+4434806734',
+          secondary_phone: '+',
+          address: '77 NE Grand Ave'
         },
         {
-          name: 'Razvan Stoenescu',
-          position: 'Developer',
+          name: 'Dave Hoyle',
+          position: 'Farm',
           avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg',
-          email: 'mailto:razvan@quasar.dev',
-          company_email: 'mailto:razvan@quasar.dev',
-          website: 'https://github.com/rstoenescu',
-          phone: '+1-004-658-0042',
-          secondary_phone: '(331) 009-4655 x3147',
-          address: '92290 Lisa Cove'
+          email: 'mailto:dave@motoperpetuofarm.com',
+          company_email: 'mailto:info@motoperpetuofarm.com',
+          website: 'https://motoperpeutofarm.com',
+          phone: '+1112223344',
+          secondary_phone: '',
+          address: '45525 SW Plumlee Rd'
         },
         {
-          name: 'Jeff Galbraith',
-          position: 'Developer',
+          name: 'Tom Grubbs',
+          position: 'Sales',
           avatar: 'https://cdn.quasar.dev/team/jeff_galbraith.jpg',
-          email: 'mailto:jeff@quasar.dev',
-          company_email: 'mailto:jeff@quasar.dev',
-          website: 'http://jeffgalbraith.dev/',
-          phone: '175.718.4633 x878',
-          secondary_phone: '175.718.4633 x878',
-          address: 'Calgary, Canada'
-        },
-        {
-          name: 'Brunhilde Panswick',
-          position: 'Administrator',
-          avatar: 'https://cdn.quasar.dev/img/avatar2.jpg',
-          email: 'test.@quasar.dev',
-          company_email: 'test.@quasar.dev',
-          website: 'http://test1.dev/',
-          phone: '175.718.4633 x878',
-          secondary_phone: '175.718.4633 x878',
-          address: 'Calgary, Canada'
-        },
-        {
-          name: 'Winfield Stapforth',
-          position: 'Administrator',
-          avatar: 'https://cdn.quasar.dev/img/avatar6.jpg',
-          email: 'test2.@quasar.dev',
-          company_email: 'test.@quasar.dev',
-          website: 'http://test2.dev/',
-          phone: '175.718.4633 x878',
-          secondary_phone: '175.718.4633 x878',
-          address: 'Calgary, Canada'
+          email: 'mailto:sales@motoperpetuofarm.com',
+          company_email: '',
+          website: 'https://motoperpetuofarm.com',
+          phone: '6667778899',
+          secondary_phone: '',
+          address: '2121 SE Belmont St'
         },
       ],
       favorites_list: [
         {
-          name: 'Pratik Patel',
+          name: 'Jack Elliott',
           position: 'Developer',
           avatar: 'https://avatars2.githubusercontent.com/u/34883558?s=400&v=4'
         },
         {
-          name: 'Razvan Stoenescu',
-          position: 'Developer',
+          name: 'Dave Hoyle',
+          position: 'Farm',
           avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg'
         },
         {
-          name: 'Jeff Galbraith',
-          position: 'Developer',
+          name: 'Tom Grubbs',
+          position: 'Sales',
           avatar: 'https://cdn.quasar.dev/team/jeff_galbraith.jpg'
         },
       ],
