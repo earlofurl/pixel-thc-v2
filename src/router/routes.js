ac0b2d5fe934c9bafebca54f65694d12b8dedf02
Jack Elliott <jackthename@gmail.com>
2020-10-28 00:26:17 -0700

    First steps adding contacts view

--- src/router/routes.js
+++ src/router/routes.js
@@ -116,6 +116,19 @@ const routes = [
       }
     ]
   },
+  {
+    path: "/contacts",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("pages/Contacts.vue"),
+        meta: {
+          requiresAuth: true
+        }
+      }
+    ]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
